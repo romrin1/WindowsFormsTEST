175be50d256fd530987cce7199c925a30180cbd3
romrin1 <59811242+romrin1@users.noreply.github.com>
2022-05-23 04:07:24 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "С руководителем";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(381, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Суммарную зарплату в разрезе департаментов",
+            "Департамент, в котором у сотрудника зарплата максимальна",
+            "Зарплаты руководителей департаментов (по убыванию)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(381, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.DropDownClosed += new System.EventHandler(this.comboBox1_DropDownClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 243);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=test;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            build_grid();
+        }
+        private void build_grid()
+        {
+            try
+            {
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter("sel_dep_im", con))
+                {
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    adapter.SelectCommand.Parameters.Add("@id", SqlDbType.Int).Value = comboBox1.SelectedIndex;
+                    adapter.SelectCommand.Parameters.Add("@ch", SqlDbType.Int).Value = checkBox1.Checked ? "1" : "0";
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    dataGridView1.Columns.Clear();
+                    foreach (DataColumn col in table.Columns)
+                    {
+                        DataGridViewColumn dcol = new DataGridViewTextBoxColumn();
+                        dcol.SortMode = DataGridViewColumnSortMode.NotSortable;
+                        dcol.DataPropertyName = col.ColumnName;
+                        dcol.HeaderText = col.ColumnName;
+                        dcol.Name = col.ColumnName;
+
+                        if (col.DataType == typeof(double))
+                        {
+                            dcol.DefaultCellStyle.Format = "N2";
+                        }
+
+                        dataGridView1.Columns.Add(dcol);
+                    }
+
+                    dataGridView1.DataSource = table;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message + " " + ex.InnerException);
+            }
+        }
+
+        //закрытие приложения
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.Close();
+        }
+
+
+
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != 0)
+                checkBox1.Visible = false;
+            else
+                checkBox1.Visible = true;
+            build_grid();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            build_grid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
